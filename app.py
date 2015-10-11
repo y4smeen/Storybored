76210db34bae72b1656fa50f1b456813d1610a89
angelachan64 <angelachan64@gmail.com>
2015-10-11 14:15:10 -0400

    fixed button and made app run on localhost:8000

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template
 
 
@@ -7,6 +8,10 @@ app = Flask(__name__)
 def about():
     return render_template("home.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run('0.0.0.0',port=8000)
