8ab547d2b7eb389d91cc62dfd1f3989e11eecfc6
angelachan64 <angelachan64@gmail.com>
2015-10-17 17:18:14 -0400

    edited css and homepage

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home2.html")
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
